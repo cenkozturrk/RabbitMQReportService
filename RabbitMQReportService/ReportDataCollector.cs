ae1807e9d6665b2046ed4ec11ff093cd7e376d93
Cenk Öztürk <ccenkozturrk@outlook.com>
2022-09-03 15:05:37 +0300

    Add project files.

---
+++ RabbitMQReportService/ReportDataCollector.cs
@@ -0,0 +1,29 @@
+﻿using Plain.RabbitMQ;
+
+namespace RabbitMQReportService
+{
+    public class ReportDataCollector : IHostedService
+    {
+        private  ISubscriber subscriber { get; }
+        public ReportDataCollector(ISubscriber subscriber)
+        {
+            this.subscriber = subscriber;
+        }       
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            subscriber.Subscribe(ProcessMessage);
+            return Task.CompletedTask;
+        }
+
+        private bool ProcessMessage(string message, IDictionary<string,object> headers)
+        {
+            return true;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RabbitMQReportService/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Plain.RabbitMQ;
+using RabbitMQ.Client;
+
+namespace RabbitMQReportService
+{
+    public static class ServiceRegistration
+    {
+        public static void AddService(this IServiceCollection services)
+        {
+            services.AddSingleton<IConnectionProvider>(new ConnectionProvider("ampq://guest@localhost:5672"));
+            services.AddScoped<ISubscriber>(x => new Subscriber(x.GetService<IConnectionProvider>(),
+                "report_exchange",
+                "report_queue",
+                "report.*",
+               ExchangeType.Topic));
+        }
+    }
+}
